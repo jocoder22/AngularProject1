bc4ec5afa5bb824c3a55d245bf60778675f794fd
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-03-07 21:26:15 -0500

    feat: creat new class

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { ImgComponent } from './img.component';
 
 @NgModule({
     imports:      [ 
         BrowserModule 
     ],
     declarations: [ 
-        AppComponent
+        AppComponent,
+        ImgComponent
     ],
-    bootstrap: [ AppComponent ]
+    bootstrap: [ AppComponent, ImgComponent ]
 })
 export class AppModule { }
